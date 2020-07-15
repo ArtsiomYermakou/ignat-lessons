9fa62c45f44785970f7d9c94a3b3bf3f2dacce1d
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-07-15 14:16:40 +0300

    Updates

---
+++ precache-manifest.14dae243e8e6b1cf5fabedd3aad1a443.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a05df76d33ba1af0034e8c45b1a7a7",
+    "url": "//github.com/ArtsiomYermakou/ignat-lessons/index.html"
+  },
+  {
+    "revision": "1ddb373a0f9b99d65fcd",
+    "url": "//github.com/ArtsiomYermakou/ignat-lessons/static/css/main.818e8e01.chunk.css"
+  },
+  {
+    "revision": "0ff1dab4a8334d253ade",
+    "url": "//github.com/ArtsiomYermakou/ignat-lessons/static/js/2.74573fdd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "//github.com/ArtsiomYermakou/ignat-lessons/static/js/2.74573fdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ddb373a0f9b99d65fcd",
+    "url": "//github.com/ArtsiomYermakou/ignat-lessons/static/js/main.034dfb01.chunk.js"
+  },
+  {
+    "revision": "9e4bde6d0ca34e95aeb0",
+    "url": "//github.com/ArtsiomYermakou/ignat-lessons/static/js/runtime-main.2d4f16d2.js"
+  }
+]);
