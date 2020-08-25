f7afa39831184bdfbfdc4b294b46330cfc633eda
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-08-25 15:42:02 +0300

    Updates

--- precache-manifest.6a5932d9b3a718254e536b5570495f39.js
+++ precache-manifest.6a5932d9b3a718254e536b5570495f39.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f7bac1a26e2756bb49fc2d8a0d48bda5",
+    "revision": "168629b9a0edcd43a70c4314d4f222ab",
     "url": "/ignat-lessons/index.html"
   },
   {
-    "revision": "c30074c4f11525be09cf",
+    "revision": "a3b12c0b96a4ab0599c4",
     "url": "/ignat-lessons/static/css/main.96383eaf.chunk.css"
   },
   {
-    "revision": "3498b7bc2883a49dc3cf",
-    "url": "/ignat-lessons/static/js/2.72200ee6.chunk.js"
+    "revision": "0373793c1884cf6f644b",
+    "url": "/ignat-lessons/static/js/2.ebb68a71.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/ignat-lessons/static/js/2.72200ee6.chunk.js.LICENSE.txt"
+    "url": "/ignat-lessons/static/js/2.ebb68a71.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c30074c4f11525be09cf",
-    "url": "/ignat-lessons/static/js/main.9986efc4.chunk.js"
+    "revision": "a3b12c0b96a4ab0599c4",
+    "url": "/ignat-lessons/static/js/main.a8d75ee2.chunk.js"
   },
   {
     "revision": "c8d31a82a1ebe2ffecc8",
