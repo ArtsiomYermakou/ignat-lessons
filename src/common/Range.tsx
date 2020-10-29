c3d0c7af9c096f812eab61ffc39531a8dd7f9ec1
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-10-29 21:07:55 +0300

    Refactor Range

--- src/common/Range.tsx
+++ src/common/Range.tsx
@@ -9,9 +9,7 @@ type PropsType = {
 }
 
 const Range = React.memo((props: PropsType) => {
-
     const [changeRange, setChangeRange] = useState<number>(props.value)
-
     return (
         <div className={style.slider}>
             <input className={style.fader} type="range" min={props.minValue} max={props.maxValue} value={changeRange}
